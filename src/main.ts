5e2aef4d3cdd38bdb8e3f3eceda741e9e5c02f87
erik-martins <erik.ozzetti.martins@gmail.com>
2024-11-09 22:03:18 -0300

    feat: config  file main swagger  validation and enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
+import 'dotenv/config';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const logger = new Logger('Main');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+
+  app.enableVersioning();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Taxi ride api')
+    .setDescription('The taxi ride API description')
+    .setVersion('1.0')
+    .addTag('taxi')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await await app.listen(Number(process.env.PORT) || 3001);
+
+  logger.log(`Application listening on: ${await app.getUrl()}`);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  logger.error(error);
+
+  process.exit(1);
+});
